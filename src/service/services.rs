1b00276166c998224e007e2550c174c97c192922
jerald <113163417+jerald06@users.noreply.github.com>
2024-01-24 16:29:17 +0530

    Update services.rs

--- src/service/services.rs
+++ src/service/services.rs
@@ -36,7 +36,6 @@ async fn add_ground_details(
 async fn get_ground_details(path: web::Path<String>, pool: web::Data<MySqlPool>) -> HttpResponse {
     let ground_name = path.into_inner(); // Extract the String from web::Path
 
-    // Prepare and execute the SQL query to fetch ground details by ID
     let result = sqlx::query_as::<_, GroundDetails>(
         r#"
         SELECT ground_id, ground_name, ground_address FROM grounds_details WHERE ground_name = ?
@@ -46,7 +45,6 @@ async fn get_ground_details(path: web::Path<String>, pool: web::Data<MySqlPool>)
     .fetch_one(pool.get_ref())
     .await;
 
-    // Handle the result and return the appropriate response
     match result {
         Ok(ground) => HttpResponse::Ok().json(ground),
         Err(sqlx::Error::RowNotFound) => HttpResponse::NotFound().body("Ground not found"),
@@ -70,7 +68,7 @@ async fn update_ground_details(
     let updated_ground_name = &updated_ground.ground_name;
     let updated_ground_address = &updated_ground.ground_address;
 
-    // Prepare and execute the SQL query to update ground details by ID
+  
     let result = sqlx::query(
         r#"
         UPDATE grounds_details SET ground_id = ?, ground_name = ?, ground_address = ? WHERE ground_id = ?
@@ -83,7 +81,6 @@ async fn update_ground_details(
         .execute(pool.get_ref())
         .await;
 
-    // Handle the result and return the appropriate response
     match result {
         Ok(_) => HttpResponse::Ok().body("Ground details updated successfully"),
         Err(err) => {
@@ -93,9 +90,9 @@ async fn update_ground_details(
         }
     }
 }
+
 #[get("/get_all_grounds")]
 async fn get_all_grounds(pool: web::Data<MySqlPool>) -> HttpResponse {
-    // Prepare and execute the SQL query to fetch all ground details
     let result = sqlx::query_as::<_, GroundDetails>(
         r#"
         SELECT ground_id, ground_name, ground_address FROM grounds_details
@@ -104,7 +101,6 @@ async fn get_all_grounds(pool: web::Data<MySqlPool>) -> HttpResponse {
     .fetch_all(pool.get_ref())
     .await;
 
-    // Handle the result and return the appropriate response
     match result {
         Ok(grounds) => HttpResponse::Ok().json(grounds),
         Err(err) => {
@@ -131,8 +127,7 @@ async fn delete_ground_by_name(
     .bind(&ground_name)
     .execute(pool.get_ref())
     .await;
-
-    // Handle the result and return the appropriate response
+    
     match result {
         Ok(_) => HttpResponse::Ok().body("Ground details deleted successfully"),
         Err(err) => {
